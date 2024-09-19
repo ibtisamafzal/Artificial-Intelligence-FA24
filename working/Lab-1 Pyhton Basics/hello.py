8b6e26522d521a92b0d4f9d6094410a1049c8e39
Muhammad Ibtisam Afzal <chaudhryibtisam2003@gmail.com>
2024-09-19 19:13:01 +0500

    Add files via upload

---
+++ working/Lab-1 Pyhton Basics/hello.py
@@ -0,0 +1,10 @@
+# print("Hello, world")
+
+x='conjdjdkdfndjede'
+y='ali'
+print(x+y)
+
+# Escape characters
+# Lists
+# Sets                                                               
+# Dictionary                           
